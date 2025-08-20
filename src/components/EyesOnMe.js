1a583128c18f2d96be6320bf48ae98eb927c2a1e
Roy Koigu <roykoigu@protonmail.com>
2025-08-20 05:38:24 +0300

    add EyesOnMe and update Keypad components with event handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,8 @@
 // Code EyesOnMe Component Here
+export default function EyesOnMe() {
+    return(
+        <div>
+            <button onFocus={()=> console.log("Good!")} onBlur={()=> console.log("Hey! Eyes on me!")}>Eyes on me</button>
+        </div>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,12 @@
 // Code Keypad Component Here
 
+
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={() => console.log("Entering password...")}/>
+
+        </div>
     )
 }
 
